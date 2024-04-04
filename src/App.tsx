b405fa5803ff8a9b9adb2839017416269d1d7f67
egutierrez53 <egutierrez53@ucmerced.edu>
2024-04-04 09:30:56 -0700

    Update styling in App.tsx, ProjectItem.tsx, and Contact.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { NavItem } from "./components";
 function App() {
   return (
     <>
-      <nav className="w-full max-w-full flex flex-row h-16 sm:h-32 justify-between sm:px-5 bg-black fixed top-0 z-50 border-b-2 border-white">
+      <nav className="w-full max-w-full flex flex-row h-16 sm:h-32 justify-between sm:px-5 bg-black sticky top-0 z-50 border-b-2 border-white">
         <ul className="w-full flex flex-row no-wrap gap-3 sm:gap-10 items-center justify-center sm:justify-end">
           <NavItem item={"About"} />
           <NavItem item={"Experience"} />
@@ -19,7 +19,6 @@ function App() {
         <Experience />
         <br></br>
         <Projects />
-        <br></br>
         <Contact />
       </main>
     </>
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,31 +1,31 @@
 const Contact = () => {
   return (
     <div className="w-full h-screen gap-3 sm:gap-10 flex flex-col justify-center items-center bg-accent">
-      <h1 id="Contact" className="text-xl font-bold text-white text-center">
+      <h1 id="Contact" className="text-xl sm:text-3xl font-bold text-white text-center">
         Contact Me
       </h1>
-      <div className="w-3/4 flex flex-col items-center justify-center">
-        <label htmlFor="email" className="form-label font-bold self-start text-white">
+      <div className="w-3/4 sm:w-2/5 flex flex-col items-center justify-center">
+        <label htmlFor="email" className="form-label font-bold self-start text-white sm:text-3xl">
           Your Email:
         </label>
         <input
           name="email"
           type="email"
-          className="form-input w-full px-1 py-1 shadow-md border-2 outline-black text-black bg-white"
+          className="form-input w-full px-1 py-1 shadow-md border-2 outline-black text-black sm:text-3xl bg-white"
         />
       </div>
-      <div className="w-3/4 flex flex-col items-center justify-center">
-        <label htmlFor="body" className="form-label font-bold self-start text-white">
+      <div className="w-3/4 sm:w-2/5 flex flex-col items-center justify-center">
+        <label htmlFor="body" className="form-label font-bold self-start text-white sm:text-3xl">
           Message:
         </label>
         <textarea
           name="body"
-          className="form-textarea w-full px-1 py-1 shadow-md border-2 outline-black text-black bg-white resize-none"
-          rows={12}
+          className="form-textarea w-full px-1 py-1 shadow-md border-2 outline-black text-black sm:text-3xl bg-white resize-none"
+          rows={14}
         />
       </div>
       <div className="w-full flex flex-col items-center justify-center">
-        <button className="w-2/5 sm:w-1/4 p-1 shadow-md font-bold bg-primary text-white">Send</button>
+        <button className="w-2/5 sm:w-1/4 p-1 shadow-md font-bold bg-primary text-white sm:text-3xl">Send</button>
       </div>
     </div>
   );
